20d894a8db3652dd1c61ba3eb703739ed6ceeda6
Marat Nepomnyashy <maratbn@gmail.com>
2010-04-22 00:11:47 -0700

    Indentation adjustment.

--- FILEfox/chrome/content/FILEfox.js
+++ FILEfox/chrome/content/FILEfox.js
@@ -30,11 +30,11 @@
  */
 
 if (!window.extension_FILEfox) {
-    window.extension_FILEfox = {
-            about: function() {
-                let string_bundle = document.getElementById('extension_FILEfox_string_bundle');
-                let text = string_bundle.getString('FILEfox.disclamer');
-                alert(text);
-            }
-        }
+    window.extension_FILEfox =  {
+                                    about: function() {
+                                        let string_bundle = document.getElementById('extension_FILEfox_string_bundle');
+                                        let text = string_bundle.getString('FILEfox.disclamer');
+                                        alert(text);
+                                    }
+                                }
 }
