c0790296a118c0f3895b050d52fa63d3f5284d03
Marat Nepomnyashy <maratbn@gmail.com>
2010-04-22 00:19:13 -0700

    Now displaying the version string in the 'About' dialog box.

--- FILEfox/chrome/content/FILEfox.js
+++ FILEfox/chrome/content/FILEfox.js
@@ -34,7 +34,7 @@ if (!window.extension_FILEfox) {
                                     about: function() {
                                         let string_bundle = document.getElementById('extension_FILEfox_string_bundle');
                                         let text = string_bundle.getString('FILEfox.disclamer');
-                                        alert(text);
+                                        alert("FILEfox version " + nsFILEfox.getVersion() + "\r\n\r\n" + text);
                                     }
                                 }
 }
