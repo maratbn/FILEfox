7c5480111effa8cc7c72115e498613961c351c8d
Marat Nepomnyashy <maratbn@gmail.com>
2010-04-25 14:35:43 -0700

    Added 3-rd party JavaScript detection to the demo site.

---
+++ website/testXSSdetection.js
@@ -0,0 +1,24 @@
+// This JavaScript file is used to test the FILEfox 3-rd party JavaScript / cross-site scripting (XSS) detection mechanism.
+
+function doLoadFileViaXSS() {
+    var text_file = window.nsFILEfox.requestLoadASCIIFile(
+                                        [
+                                            "This is the FILEfox demonstration website.  ",
+                                            "FILEfox has a security feature that warns the user when it detects 3-rd party JavaScript -- that is JavaScript coming from some server other than the server serving the main webpage.  ",
+                                            "This file loading request is coming from a server address different from the main webpage address, and therefore this dialog box should display such a warning in addition to this message from the demo site.  ",
+                                            "3-rd party JavaScript can be legitimate, but it is also known to be used by malicious servers for cross-site scripting (XSS) attacks."
+                                        ].join(""));
+    if (!text_file) {
+        alert("Demonstration website:  Could not get a file via the FILEfox Firefox extension.  The user has either canceled, there was an error in some part of the loading process.");
+        return;
+    }
+
+    alert(
+        [
+            "Demonstration website:  ",
+            "File loading has been approved by the user.  ",
+            "Had this 3-rd party JavaScript been malicious, it would likely be uploading your file to its malicious server by now."
+        ].join(""));
+
+    text_file_view.displayTextFile(text_file);
+}
