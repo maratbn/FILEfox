7bbd7916a6e66e8fcaf278a7e0aea62ea08465f4
Marat Nepomnyashy <maratbn@gmail.com>
2010-04-27 02:10:42 -0700

    Synchronized the XSS test file with the FILEfox dialog box message.

--- website/testXSSdetection.js
+++ website/testXSSdetection.js
@@ -4,9 +4,10 @@ function doLoadFileViaXSS() {
     var text_file = window.nsFILEfox.requestLoadASCIIFile(
                                         [
                                             "This is the FILEfox demonstration website.  ",
-                                            "FILEfox has a security feature that warns the user when it detects 3-rd party JavaScript -- that is JavaScript coming from some server other than the server serving the main webpage.  ",
-                                            "This file loading request is coming from a server address different from the main webpage address, and therefore this dialog box should display such a warning in addition to this message from the demo site.  ",
-                                            "3-rd party JavaScript can be legitimate, but it is also known to be used by malicious servers for cross-site scripting (XSS) attacks."
+                                            "FILEfox has a security feature that warns the user when it detects 3-rd party JavaScript -- that is JavaScript coming from some domain other than the main webpage domain.  ",
+                                            "This file loading request is coming from a domain different from the main webpage domain, and therefore this dialog box should display such a warning in addition to this message from the demo site.  ",
+                                            "3-rd party JavaScript can be legitimate, but it is also known to be used by malicious servers for cross-site scripting (XSS) attacks.  ",
+                                            "Such a malicious XSS \"attack\" could upload your private file to the malicious 3-rd party server."
                                         ].join(""));
     if (!text_file) {
         alert("Demonstration website:  Could not get a file via the FILEfox Firefox extension.  The user has either canceled, there was an error in some part of the loading process.");
